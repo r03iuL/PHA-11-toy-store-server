ac4dbff0c004187d1ab97e2b896fc91af1f03a61
r03iuL <robiulislam1806@gmail.com>
2023-05-23 23:00:18 +0600

    created a post function for all toys

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.get("/",(req, res)=>{
     res.send("Server is running")
 })
 
+
+
 const uri = "mongodb+srv://cse2112020031:VWhUPD2zvtEyNvz6@cluster0.by40fgv.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,10 +29,10 @@ const client = new MongoClient(uri, {
   }
 });
 
-
-
 async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
     const toyCollection = client.db("toystore").collection("toys")
 
     app.get('/alltoys',async(req,res)=>{
@@ -42,60 +44,56 @@ async function run() {
         const result =await toyCollection.find(query).toArray();
         res.send(result); 
     })
+
     app.get('/category',async(req,res)=>{
-      console.log(req.query.category);
-      let query ={}
-      if(req.query?.category){
-          query ={email:req.query.category}
-      }
-      const result =await toyCollection.find(query).toArray();
-      res.send(result); 
-  })
-  app.get("/alltoys/:id",async(req,res)=>{
-    const id = req.params.id;
-    console.log(id);
-    const query = {_id: new ObjectId(id)}
-    const result = await toyCollection.findOne(query);
-    res.send(result)
-})
-app.delete("/alltoys/:id",async(req,res)=>{
-  const id = req.params.id;
-  console.log(id);
-  const query = {_id: new ObjectId(id)}
-  const result = await toyCollection.deleteOne(query);
-  res.send(result)
-})
-app.put("/alltoys/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const reqBody = req.body;
+        console.log(req.query.category);
+        let query ={}
+        if(req.query?.category){
+            query ={email:req.query.category}
+        }
+        const result =await toyCollection.find(query).toArray();
+        res.send(result); 
+    })
 
-    const filter = { _id: new ObjectId(id) };
-    const options = { upsert: true };
-
-    const updateDoc = {
-      $set: {
-        customerName: reqBody.customerName,
-        email: reqBody.email,
-        price: reqBody.price,
-        photoURL: reqBody.photoURL,
-        sellerName: reqBody.sellerName,
-        category: reqBody.category,
-        rating: reqBody.rating,
-        quantity: reqBody.quantity,
-      },
-    };
-
-    const result = await toyCollection.updateOne(filter, updateDoc, options);
-
-    res.send(result);
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("An error occurred while updating the toy.");
-      }
-    });
-
+    app.get("/alltoys/:id",async(req,res)=>{
+        const id = req.params.id;
+        console.log(id);
+        const query = {_id: new ObjectId(id)}
+        const result = await toyCollection.findOne(query);
+        res.send(result)
+    })
+    app.delete("/alltoys/:id",async(req,res)=>{
+        const id = req.params.id;
+        console.log(id);
+        const query = {_id: new ObjectId(id)}
+        const result = await toyCollection.deleteOne(query);
+        res.send(result)
+    })
+    app.put("/alltoys/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const reqBody = req.body;
+    
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+    
+        const updateDoc = {
+          $set: {
+            customerName: reqBody.customerName,
+            email: reqBody.email,
+            price: reqBody.price,
+            photoURL: reqBody.photoURL,
+            sellerName: reqBody.sellerName,
+            category: reqBody.category,
+            rating: reqBody.rating,
+            quantity: reqBody.quantity,
+          },
+        };
+    
+        const result = await toyCollection.updateOne(filter, updateDoc, options);
+    
+        res.send(result);
+        console.log(result);
       } catch (error) {
         console.error(error);
         res.status(500).send("An error occurred while updating the toy.");
@@ -103,7 +101,16 @@ app.put("/alltoys/:id", async (req, res) => {
     });
 
 
-    finally {
+    app.post("/alltoys",async(req,res)=>{
+        const toy =req.body;
+        console.log(toy);
+        const result = await toyCollection.insertOne(toy);
+        res.send(result);
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
